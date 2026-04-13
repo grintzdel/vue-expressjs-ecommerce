b438c17833c11a51cd3e7a42fb741e59e07044ec
maoudin <mathis.oudin42@gmail.com>
2026-04-13 14:01:29 +0200

    chore: init monorepo with server and client configs

---
+++ server/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  moduleNameMapper: {
+    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
+    "^@shared/(.*)$": "<rootDir>/src/shared/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+  },
+};
+
+export default config;
